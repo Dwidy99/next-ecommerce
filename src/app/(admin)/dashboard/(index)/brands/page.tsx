6b3146db2cc1277a018a61b37d56f8ed8277100a
Dwi <98dwiyulianto@gmail.com>
2025-09-06 15:35:17 +0700

    Dashboard - Brands (CRUD) : Read (UI) 1

---
+++ src/app/(admin)/dashboard/(index)/brands/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { DataTable } from '../categories/data-table'
+import { columns } from './columns'
+
+export default function BrandPage() {
+  return (
+    <div className="space-y-4">
+			<div className="text-right">
+				<Button size="sm" className="h-8 gap-1" asChild>
+					<Link href="/dashboard/locations/create">
+						<PlusCircle className="h-3.5 w-3.5" />
+						<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+							Add Location
+						</span>
+					</Link>
+				</Button>
+			</div>
+			<Card x-chunk="dashboard-06-chunk-0">
+				<CardHeader>
+					<CardTitle>Locations</CardTitle>
+					<CardDescription>
+						Manage your Locations and view their sales performance.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+                    <DataTable columns={columns} data={[]} />
+				</CardContent>
+			</Card>
+		</div>
+  )
+}
---
+++ src/lib/supabase.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? ""
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const getImageUrl = (name: string) => {
+    const { data } = supabase
+        .storage
+        .from('e-commerce')
+        .getPublicUrl(`public/brands/${name}`)
+
+    return data.publicUrl
+}
