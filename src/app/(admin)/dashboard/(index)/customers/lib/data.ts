5f008c990a2c53079d2e4177bb0b7a5035fffe4f
Dwi <98dwiyulianto@gmail.com>
2025-09-17 16:50:07 +0700

    Customer - User : Read (Integrate)

--- src/app/(admin)/dashboard/(index)/customers/lib/data.ts
+++ src/app/(admin)/dashboard/(index)/customers/lib/data.ts
@@ -1,68 +1,34 @@
 import { prisma } from "lib/prisma";
-import { TColumn } from "../columns";
+import { TColumn } from '../columns';
 
-export async function getProducts() {
+export async function getCustomers() {
     try {
-        const products = await prisma.product.findMany({
-            orderBy: { name: 'asc' },
-            select: { 
-                    id: true, 
-                    _count: {
-                        select: {
-                            orders: true
-                        }
-                },
-                name: true,
-                created_at: true,
-                price: true,
-                stock: true,
-                category: {
+        const customers = await prisma.user.findMany({
+            where: {
+                role: "customer"
+            },
+            include: {
+                _count: {
                     select: {
-                        name: true
+                        orders: true
                     }
-                },
-                brand: {
-                    select: {
-                        name: true
-                    }
-                },
-                location: {
-                    select: {
-                        name: true
-                    }
-                },
-                images: true
+                }
             }
         })
 
-        const response_products: TColumn[] = products.map((product) => ({
-            id: product.id,
-            name: product.name,
-            product_name: product.name, // ✅ ini wajib
-            image_url: product.images[0] ?? "",
-            category: product.category?.name ?? "",
-            price: Number(product.price),
-            total_sales: product._count.orders,
-            stock: product.stock,
-            createdAt: product.created_at,
-        }));
-        
-        return response_products
+        const response: TColumn[] = customers.map((cust) => {
+            return {
+                id: cust.id,
+                name: cust.name,
+                email: cust.email,
+                total_transactions: cust._count.orders
+            }
+        })
+
+        return response;
     } catch (err) {
-        console.error(err)
-
+        console.log(err);
         return []
-    } 
+    }
     // finally {}
-}
-
-export async function getProductById(id: number) {
-    try {
-        const product = await prisma.product.findUnique({ where: { id } });
-        return product;
-    } catch (error) {
-        console.error("Error fetching product:", error);
-        return null;
-    } 
-    // finally {console.log("getProductById() selesai dijalankan");}
 }
