f49ab05796f6ab2d5e89db086b93421dccd87d64
Dwi <98dwiyulianto@gmail.com>
2025-09-22 23:12:20 +0700

    Customer Catalogs : Integrate List View

--- src/app/(customer)/(index)/catalogs/_components/filter-category.tsx
+++ src/app/(customer)/(index)/catalogs/_components/filter-category.tsx
@@ -1,49 +1,26 @@
+import { getCategories } from "@/app/(admin)/dashboard/(index)/categories/lib/data";
 import React from "react";
 
-export default function FilterCategory() {
+export default async function FilterCategory() {
+  const categories = await getCategories();
   return (
     <div className="flex flex-col gap-[14px]">
       <p className="font-semibold leading-[22px]">Category</p>
-      <label className="font-semibold flex items-center gap-3">
-        <input
-          type="checkbox"
-          name="brand"
-          className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-        />
-        <span>Smartphone</span>
-      </label>
-      <label className="font-semibold flex items-center gap-3">
-        <input
-          type="checkbox"
-          name="brand"
-          className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-        />
-        <span>Motocycle</span>
-      </label>
-      <label className="font-semibold flex items-center gap-3">
-        <input
-          type="checkbox"
-          name="brand"
-          className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-        />
-        <span>Car</span>
-      </label>
-      <label className="font-semibold flex items-center gap-3">
-        <input
-          type="checkbox"
-          name="brand"
-          className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-        />
-        <span>Foods</span>
-      </label>
-      <label className="font-semibold flex items-center gap-3">
-        <input
-          type="checkbox"
-          name="brand"
-          className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-        />
-        <span>Drinks</span>
-      </label>
+      {categories.map((category) => (
+        <label
+          key={`${category.name + category.id}`}
+          htmlFor={`${category.name + category.id}`}
+          className="font-semibold flex items-center gap-3"
+        >
+          <input
+            type="checkbox"
+            name="category"
+            value={category.id}
+            className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
+          />
+          <span>{category.name}</span>
+        </label>
+      ))}
     </div>
   );
 }
