b39b1be7384ef447fd476e6bfcd7a914254da80a
Dwi <98dwiyulianto@gmail.com>
2025-09-27 13:43:03 +0700

    Customer - Cart : Integrate Cart 1

--- src/app/(customer)/(index)/cart/_components/cart-product.tsx
+++ src/app/(customer)/(index)/cart/_components/cart-product.tsx
@@ -4,7 +4,8 @@ import { rupiahFormat } from "@/lib/utils";
 import React from "react";
 
 export default function CartProduct() {
-  const { products } = useCart();
+  const { products, decreaseQuantity, increaseQuantity, removeProduct } =
+    useCart();
   return (
     <>
       <div
@@ -38,13 +39,19 @@ export default function CartProduct() {
             <div className="w-[120px] flex flex-col gap-1">
               <p className="text-sm text-[#616369]">Quantity</p>
               <div className="flex items-center gap-3">
-                <button className="w-6 h-6 flex shrink-0">
+                <button
+                  onClick={() => decreaseQuantity(cart.id)}
+                  className="w-6 h-6 flex shrink-0"
+                >
                   <img src="/assets/icons/minus-cirlce.svg" alt="minus" />
                 </button>
                 <p className="text-[#0D5CD7] font-semibold leading-[22px]">
                   {cart.quantity}
                 </p>
-                <button className="w-6 h-6 flex shrink-0">
+                <button
+                  className="w-6 h-6 flex shrink-0"
+                  onClick={() => increaseQuantity(cart.id)}
+                >
                   <img src="/assets/icons/add-circle.svg" alt="plus" />
                 </button>
               </div>
@@ -55,7 +62,11 @@ export default function CartProduct() {
                 Rp 789.493.222
               </p>
             </div>
-            <button className="p-[12px_24px] bg-white rounded-full text-center font-semibold border border-[#E5E5E5]">
+            <button
+              type="button"
+              onClick={() => removeProduct(cart.id)}
+              className="p-[12px_24px] bg-white rounded-full text-center font-semibold border border-[#E5E5E5]"
+            >
               Remove
             </button>
           </div>
