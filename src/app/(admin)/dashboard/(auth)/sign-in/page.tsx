daccb916750316f93208d6b8dfe206c869aaaceb
Dwi <98dwiyulianto@gmail.com>
2025-11-02 12:26:05 +0700

    Dashboard - Sign In : Delete some code

--- src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
+++ src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
@@ -1,12 +1,11 @@
-import FormSignIn from "./_components/form"
+import FormSignIn from "./_components/form";
 
-export default function SignInPage({
-}: React.ComponentProps<"div">) {
+export default function SignInPage() {
   return (
     <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-          <FormSignIn/>
+        <FormSignIn />
       </div>
     </main>
-  )
+  );
 }
