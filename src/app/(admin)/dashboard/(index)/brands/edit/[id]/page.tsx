1c10d58f63aa97f4acff43f22d6e1c3351e8a906
Dwi <98dwiyulianto@gmail.com>
2025-11-02 12:35:02 +0700

    Dashboard - rands : Delete some code

--- src/app/(admin)/dashboard/(index)/brands/edit/[id]/page.tsx
+++ src/app/(admin)/dashboard/(index)/brands/edit/[id]/page.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import FormBrand from '../../_components/form-brand'
-import { Tedit } from '@/types'
-import { getBrandById } from '../../lib/data'
-import { redirect } from 'next/navigation'
+import FormBrand from "../../_components/form-brand";
+import { getBrandById } from "../../lib/data";
+import { redirect } from "next/navigation";
 
-export default async function EditPage({params}: Tedit) {
-  const { id } = await params
-  const brand = await getBrandById(id)
+export default async function EditPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const brand = await getBrandById(id);
 
-  if(!brand) {
-    return redirect("/dashboard/brands")
+  if (!brand) {
+    redirect("/dashboard/brands");
   }
 
-  return <FormBrand type='EDIT' data={brand} />
+  return <FormBrand type="EDIT" data={brand} />;
 }
